e66613cf4541921610347ec3e503bbee07154986
gururk2@gmail.com <gururk2@gmail.com>
2015-10-06 23:09:24 +0530

    nothing

--- qa-content/qa-admin.js
+++ qa-content/qa-admin.js
@@ -1,26 +1,4 @@
-/*
-	Question2Answer by Gideon Greenspan and contributors
 
-	http://www.question2answer.org/
-
-
-	File: qa-content/qa-admin.js
-	Version: See define()s at top of qa-include/qa-base.php
-	Description: Javascript for admin pages to handle Ajax-triggered operations
-
-
-	This program is free software; you can redistribute it and/or
-	modify it under the terms of the GNU General Public License
-	as published by the Free Software Foundation; either version 2
-	of the License, or (at your option) any later version.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	More about this license: http://www.question2answer.org/license.php
-*/
 
 var qa_recalc_running=0;
 
